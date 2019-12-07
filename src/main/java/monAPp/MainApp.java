112ebce97924d113542ddd08d4ea65c4f28273d1
daniel <daniel@tutoderien.com>
2019-12-07 08:13:36 +0100

    added the dao

--- src/main/java/monAPp/MainApp.java
+++ src/main/java/monAPp/MainApp.java
@@ -1,12 +1,31 @@
 package monAPp;
 
+import entities.Customer;
+import model.CustomerDAO;
+
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class MainApp {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("connect");
+
+        CustomerDAO customerDAO = new CustomerDAO();
+
+        Customer daniel = new Customer();
+        daniel.setId(1L);
+        daniel.setName("daniel");
+
+        customerDAO.saveCustomer(daniel);
+
+        System.out.println("Client: "+customerDAO.showCustomer(1L).getName());
+
+        customerDAO.deleteCustomer(1L);
+
+        System.out.println(customerDAO.showCustomer(1L).getName());
+
+
+
     }
 
 }
